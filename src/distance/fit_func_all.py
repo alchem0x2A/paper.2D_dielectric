f7febadcd908b90497fd50a42c4bd97d14cd5286
T.Tian <tian.tian@chem.ethz.ch>
2018-10-09 17:35:17 +0200

    All plots for eps and alpha done for fig1

---
+++ src/distance/fit_func_all.py
@@ -0,0 +1,88 @@
+import numpy
+from scipy.optimize import curve_fit
+import os, os.path
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from scipy.constants import pi
+from model_eps import plot_eps_para, plot_eps_perp
+plt.style.use("science")
+
+
+def fit_para(L, d, eps_2D):
+    return (eps_2D - 1) * d / L  + 1
+
+def fit_vert(L, d, eps_2D):
+    return 1 / (d / L * (1 / eps_2D - 1) + 1)
+
+root = "../../data/distance/"
+g = os.walk(root)
+names = next(g)[1]
+gs = GridSpec(3, 1)
+fig1 = plt.figure(figsize=(3, 3))
+fig2 = plt.figure(figsize=(3, 3))
+ax1_r = fig1.add_subplot(gs[0])
+ax1 = fig1.add_subplot(gs[1: ])
+ax2_r = fig2.add_subplot(gs[0])
+ax2 = fig2.add_subplot(gs[1: ])
+
+plot_eps_para(ax1, alpha_0=8.5, dev=4)
+plot_eps_perp(ax2, alpha_0=0.47, dev=0.15)
+
+for i, item in enumerate(g):
+    if "old" in item:
+        continue
+    for f in item[2]:
+        f_path = os.path.join(item[0], f)
+        if "agr" not in f_path:
+            continue
+        print(f_path)
+        data = numpy.genfromtxt(f_path,
+                                delimiter=" "
+                                # skip_header=297,
+                                # skip_footer=2
+         )
+        # data = data[1: , :]
+        L = data[:, 0]
+        eps_SL = data[:, 1]
+        if "parallel.agr" in f_path:
+            alpha_SL = L * (data[:, 1] - 1)  / (4 * pi)
+            l, *_ = ax1.plot(L, eps_SL, "o-",
+                             label=names[i],
+                             markersize=4)
+            ax1_r.plot(L, alpha_SL, "o-",
+                       markersize=4,
+                       color=l.get_c())
+        elif "perpendicular.agr" in f_path:
+            alpha_SL = L * (data[:, 1] - 1) / (data[:, 1]) / (4 * pi)
+            l, *_ = ax2.plot(L, eps_SL, "o-",
+                             label=names[i],
+                             markersize=4)
+            ax2_r.plot(L, alpha_SL, "o-",
+                       markersize=4,
+                       color=l.get_c())
+
+ax1.set_xlabel("$L$ ($\\AA$)")
+ax1_r.set_title("parallel")
+ax1.set_ylabel("$\\epsilon^{\\parallel}_{\mathrm{SL}}$")
+ax1_r.set_ylabel("$\\alpha_{\\parallel}/(4\\pi\\varepsilon_0) (\\AA)$")
+ax1_r.set_ylim(5, 13)
+ax1_r.set_xticklabels([])
+# ax1_r.set_ylim(min(alpha_SL) * 0.75, max(alpha_SL)* 1.25)
+
+    # ax.plot(LL, fit_vert(LL, *param), "--")
+ax2.set_xlabel("$L$ ($\\AA$)")
+ax2.set_ylabel("$\\epsilon^{\\perp}_{\mathrm{SL}}$")
+ax2_r.set_ylabel("$\\alpha_{\\perp}/(4\\pi \\varepsilon_0) (\\AA)$")
+ax2_r.set_title("perp")
+ax2.set_ylim(1, 4)
+    # ax.set_title("{2}, ${{\\epsilon_{{2D}}^{{\\perp}} }}={1:.3f}$ ${{d}}={0:.3f}$".format(*param, names[i]))
+ax2_r.set_ylim(0.3, 0.8)
+ax2_r.set_xticklabels([])
+# ax2.set_ylim(min(alpha_SL) * 0.75, max(alpha_SL)* 1.25)
+ax1.legend()
+ax2.legend()
+
+fig1.tight_layout()
+fig2.tight_layout()
+fig1.savefig(os.path.join("../../tmp_img/", "all-para.svg"))
+fig2.savefig(os.path.join("../../tmp_img/", "all-perp.svg"))
---
+++ src/distance/model_eps.py
@@ -0,0 +1,43 @@
+import numpy
+import matplotlib.pyplot as plt
+from scipy.constants import epsilon_0, pi
+
+def eps_para(alpha, L):
+    return 1 + 4 * pi * alpha / L
+
+def eps_perp(alpha, L):
+    return 1 / (1 - 4 * pi * alpha / L)
+
+def plot_eps_para(ax, alpha_0, dev, trans=0.7):
+    L = numpy.linspace(10, 60, 100)
+    [ax.plot(L, eps_para(a, L), c="#839abf",
+             alpha=(1 - abs(a - alpha_0) / dev) * trans) \
+     for a in numpy.linspace(alpha_0 - dev * 0.999,
+                             alpha_0 + dev * 0.999)]
+    return
+
+def plot_eps_perp(ax, alpha_0, dev, trans=0.7):
+    L = numpy.linspace(10, 60, 100)
+    [ax.plot(L, eps_perp(a, L), c="#cca384",
+             alpha=(1 - abs(a - alpha_0) / dev) * trans) \
+     for a in numpy.linspace(alpha_0 - dev * 0.999,
+                             alpha_0 + dev * 0.999)]
+    return
+
+plt.style.use("science")
+fig = plt.figure(figsize=(6, 2.5))
+ax = fig.add_subplot(121)
+ax2 = fig.add_subplot(122)
+L = numpy.linspace(10, 60, 100)
+#color plots
+[ax.plot(L, eps_para(a, L), c="#839abf", alpha=(1 - abs(a - 9.0) / 4)) for a in numpy.linspace(5, 13, 100)]
+ax.set_title("$\\alpha^{\\parallel} = 9 \\pm 4$")
+ax.set_xlabel("L")
+ax.set_ylabel("Model $\\varepsilon^{\\parallel}$")
+# 
+[ax2.plot(L, eps_perp(a, L), c="#cca384", alpha=(1 - abs(a - 0.5) / 0.1)) for a in numpy.linspace(0.4, 0.6, 100)]
+ax2.set_title("$\\alpha^{\\perp} = 0.5 \\pm 0.1$")
+ax2.set_xlabel("L")
+ax2.set_ylabel("Model $\\varepsilon^{\\perp}$")
+fig.tight_layout()
+fig.savefig("../../tmp_img/model_eps_parallel.svg")
